362912c55064fcbf1110aa79a32dd38d0f795d91
Mike <mike>
2020-08-28 21:47:37 -0500

    progress

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent'
 
 class Menu extends Component {
     constructor(props) {
@@ -56,9 +57,7 @@ class Menu extends Component {
             <div className="row">
               {menu}
             </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
-            </div>
+            <Dishdetail dish={this.state.selectedDish}/>
           </div>
         );
     }
