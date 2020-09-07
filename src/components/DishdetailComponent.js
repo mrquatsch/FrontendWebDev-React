0bad952d15a94d7ec9d11717d59a0ce2d4cf9a84
Mike Sterry <msterry@spscommerce.com>
2020-09-07 16:52:00 -0500

    assignment 3 - partially through task 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, 
+    Label, Breadcrumb, BreadcrumbItem, Col, Row, Button } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
@@ -53,30 +55,88 @@ import { Link } from 'react-router-dom';
         }
     }
 
+    function CommentForm() {
+        return (
+            <React.Fragment>
+                <Button outline>
+                    <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                </Button>
+                {/* <CommentModal /> */}
+            </React.Fragment>
+        )
+    }
+
+    function CommentModal() {
+        return (
+            <div className="col-12 col-md-9">
+                <LocalForm>
+                    <Row className="form-group">
+                        <Label htmlFor="firstname" md={2}>Rating</Label>
+                        <Col md={10}>
+                            <Control.select model=".rating" name="rating"
+                                className="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                            </Control.select>
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Label htmlFor="yourname" md={2}>Your Name</Label>
+                        <Col md={10}>
+                            <Control.text model=".yourname" id="yourname" name="yourname"
+                                placeholder="Your Name"
+                                className="form-control"
+                                />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Label htmlFor="message" md={2}>Comment</Label>
+                        <Col md={10}>
+                            <Control.textarea model=".comment" id="comment" name="comment"
+                                rows="6"
+                                className="form-control" />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col md={{size:10, offset: 2}}>
+                            <Button type="submit" color="primary">
+                            Send Feedback
+                            </Button>
+                        </Col>
+                    </Row>
+                </LocalForm>
+            </div>
+        )
+    }
 
     const  DishDetail = (props) => {
         const dish = props.dish
         if (dish != null && dish !== undefined) {
             return (
                 <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1" name="dishDetail">
-                        <RenderDish dish={props.dish} />
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
                     </div>
-                    <div className="col-12 col-md-5 m-1" name="DishComment">
-                        <RenderComments comments={props.comments} />
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1" name="dishDetail">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1" name="DishComment">
+                            <RenderComments comments={props.comments} />
+                            <CommentForm />
+                        </div>
                     </div>
-                </div>
                 </div>
             );
         } else {
